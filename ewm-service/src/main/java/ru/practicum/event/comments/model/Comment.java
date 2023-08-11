70770eb3b99314f5a97e9d95a5c8e407bd1ba513
kidnaped <trinadesiat@yandex.ru>
2023-08-11 10:26:19 +0300

    feature: add Comment functionality base structure, model and dto classes

---
+++ ewm-service/src/main/java/ru/practicum/event/comments/model/Comment.java
@@ -0,0 +1,31 @@
+package ru.practicum.event.comments.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import ru.practicum.dto.Utils;
+import ru.practicum.event.model.Event;
+import ru.practicum.user.model.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String text;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private User author;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Event event;
+    @JsonFormat(pattern = Utils.FORMAT)
+    private LocalDateTime created;
+}
